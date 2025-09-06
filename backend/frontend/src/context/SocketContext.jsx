baed56da244eb7d184ec6cc4950b0551ad67b396
karthik <karthikbongu23@gmail.com>
2025-09-06 14:47:33 +0530

    Finally

--- backend/frontend/src/context/SocketContext.jsx
+++ backend/frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4002", {
+      const socket = io("https://chatapplication-z1o9.onrender.com", {
         
         query: {
           userId: authUser.user._id,
